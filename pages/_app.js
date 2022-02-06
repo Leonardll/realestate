1af06dd0307722026f9ff3fdc06802e174914fbd
Leonard <lonardlator@yahoo.fr>
2022-02-06 12:13:38 +0100

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.scss'
 import '../styles/custom.scss'
 import Head from 'next/head'
 //import NProgress from 'nprogresfa-spin'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import React from 'react'
 
 function MyApp({ Component, pageProps }) {
