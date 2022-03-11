5ad30990eed3330bdf446e5cfe7a9dbe4173c304
Leonard <lonardlator@yahoo.fr>
2022-03-11 14:22:15 +0100

    minor

--- components/Services.js
+++ components/Services.js
@@ -1,57 +1,57 @@
-
-import Image from 'next/image'
-import React from 'react'
-import Hands from '../assets/images/DSC_8081.JPG'
-import Boxman from '../assets/images/DSC_8155.jpg'
-
+import Image from "next/image";
+import React from "react";
+import Hands from "../assets/images/DSC_8081.JPG";
+import Boxman from "../assets/images/DSC_8155.jpg";
 
 export const Services = () => {
-    const myLoader = ({ src, width, quality }) => {
-        return `${src}?w=${width}&q=${
-          quality || 75
-        }`;
-    }
+  const myLoader = ({ src, width, quality }) => {
+    return `${src}?w=${width}&q=${quality || 75}`;
+  };
 
   return (
-      <div className='section my-2'>
-          <div className="mt-2 d-flex justify-content-center">
-              <h1 className="my-3" id="services">Services</h1>
+    <div className="section my-2">
+      <div className="container">
+        <div className="mt-2 d-flex justify-content-center">
+          <h1 className="my-3" id="services">
+            Services
+          </h1>
+        </div>
+        <div className="row d-flex justify-content-between">
+          <div className="col-12 col-md-6 p-3">
+            <Image
+              className="card-img-top img-fluid"
+              src={Hands}
+              loader={myLoader}
+              layout="responsive"
+              objectFit="cover"
+              fill="cover"
+              height={300}
+              width={500}
+              alt="bell"
+            />
+            <div className="p-3 border text-capitalize">
+              <h3>conciergerie</h3>
+            </div>
           </div>
-    <div className="row d-flex justify-content-between">
-    <div className="col-12 col-md-6 p-3">
-        <Image 
-        className='card-img-top img-fluid'
-        src={Hands}
-        loader={myLoader} 
-        layout='responsive'
-        objectFit='cover'
-        fill='cover' 
-        height={300} 
-        width={500} 
-        alt='bell' />
-     <div className="p-3 border text-capitalize">
-         <h3>conciergerie</h3>
-    </div>
-    </div>
-    <div className="col-12 col-md-6 p-3">
-    <Image 
-        className='card-img-top img-fluid'
-        src={Boxman} 
-        loader={myLoader}
-        layout='responsive'
-        objectFit='cover'
-        //fill='cover' 
-        height={300} 
-        width={500} 
-        alt='bell' />
-      <div className="p-3 border text-capitalize">
-          <h3>real estate</h3>
-      </div>
-      <div className="card-text">
-          
+          <div className="col-12 col-md-6 p-3">
+            <Image
+              className="card-img-top img-fluid"
+              src={Boxman}
+              loader={myLoader}
+              layout="responsive"
+              objectFit="cover"
+              //fill='cover'
+              height={300}
+              width={500}
+              alt="bell"
+            />
+            <div className="p-3 border text-capitalize">
+              <h3>real estate</h3>
+            </div>
+            <div className="card-text"></div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-      </div>  
-      )
-}
+  );
+};
