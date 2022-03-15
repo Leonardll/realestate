04e49ac709a5089f45fe799e164bf77867a64a6a
Leonard <lonardlator@yahoo.fr>
2022-03-15 12:17:52 +0100

    main

--- pages/api/listing.js
+++ pages/api/listing.js
@@ -11,23 +11,24 @@ export default function Propertyhandler(req, res) {
   var parser = new xml2js.Parser({explicitArray : false});
   const data = fs.readFileSync(path.join(process.cwd(),'listing.xml'))
   parser.parseString(data,function (err, results){
-    //console.log(results.client.secondhandListing.property)
+
+    results = results.client.secondhandListing.property
     res.status(200).json({results})
   })
 
 }
 
-export  function getAllPropertyhandler(req, res) {
-  const {operationType} = req.query
-  const queryObject = {}
-  operationType ? queryObject.operationType = operationType === 'rent' || 'sale' : null
-  console.log(queryObject)
-  var parser = new xml2js.Parser({explicitArray : false});
-  const data = fs.readFileSync(path.join(process.cwd(),'listing.xml'))
-  parser.parseString(data,function (err, results){
-    results = results.client.secondhandListing.property.find(queryObject)
-    //console.log(results.client.secondhandListing.property)
-    res.status(200).json({results})
-  })
+// export  function getAllPropertyhandler(req, res) {
+//   const {operationType} = req.query
+//   const queryObject = {}
+//   operationType ? queryObject.operationType = operationType === 'rent' || 'sale' : null
+//   console.log(queryObject)
+//   var parser = new xml2js.Parser({explicitArray : false});
+//   const data = fs.readFileSync(path.join(process.cwd(),'listing.xml'))
+//   parser.parseString(data,function (err, results){
+//     results = results.client.secondhandListing.property.find(queryObject)
+//     //console.log(results.client.secondhandListing.property)
+//     res.status(200).json({results})
+//   })
 
-}
+// }
