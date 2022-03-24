2eb8bf542f00188bd9cea8abe303af2454357cde
Leonard <lonardlator@yahoo.fr>
2022-03-25 00:47:01 +0100

    main

--- components/property.js
+++ components/property.js
@@ -13,14 +13,14 @@ const Properties = () => (
 
   <Link href="https://www.idealista.com/pro/unico-hogar/venta-viviendas/" passHref >
     <div className=" banner d-flex align-items-center justify-content-center col-12 col-md-6 p-3">
-      <h3 className="text-center text-capitalize text-white">Buy</h3>
+      <h3 className="text-center text-capitalize text-hogar2">Buy</h3>
     </div>
   </Link>
   
   
   <Link href="https://www.idealista.com/pro/unico-hogar/alquiler-viviendas/" passHref >
     <div className=" banner d-flex align-items-center justify-content-center col-12 col-md-6 p-3 bg-white">
-      <h3 className="text-center text-capitalize">Rent</h3>
+      <h3 className="text-center text-capitalize text-hogar">Rent</h3>
     </div>
   </Link>
     </div>
