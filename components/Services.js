56aecdfbf82280a4d0fd3dcb76dfcbffbfcc56fc
Leonard <lonardlator@yahoo.fr>
2022-03-24 11:37:40 +0100

    main

--- components/Services.js
+++ components/Services.js
@@ -36,7 +36,7 @@ export const Services = () => {
             </div>
           </div>
           </Link>
-          <Link href={'/ourServices/propertyManagement'} passHref>
+          <Link href={'/ourServices/property-management'} passHref>
           <div className="col-12 col-md-6 p-3">
             <Image
               className="card-img-top img-fluid"
