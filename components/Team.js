c76b0764b3b07e6912cc754d296dfdba14528776
Leonard <lonardlator@yahoo.fr>
2022-03-25 11:09:03 +0100

    main

--- components/Team.js
+++ components/Team.js
@@ -21,7 +21,7 @@ export const Team = ({isVisible}) => {
         initial={{ opacity: 0, x: 150 }}
         whileInView={{ opacity: 1, x:0 }}
         //animate={{ opacity: 1, x:0}}
-        transition={{duration: 1, ease:"easeOut",delay:.5}}
+        transition={{duration: 1, ease:"easeInOut",delay:.3}}
         //viewport={{ once: true }} 
         className="row g-4 d-flex flex-row p-3">
           <div className="col-lg-4 d-flex justify-content-center">
@@ -54,7 +54,7 @@ export const Team = ({isVisible}) => {
         initial={{ opacity: 0, x: -150 }}
         whileInView={{opacity: 1, x:0 }}
         //animate={{ opacity: 1, x:[100,0]}}
-        transition={{duration: 1, ease:"easeOut",delay:.5}}
+        transition={{duration: 1, ease:"easeOut",delay:.3}}
         className="row d-flex flex-row-reverse p-3">
           <div className="col-lg-4 flex-row-reverse justify-content-center">
             <div className="h-100 text-center">
--- components/property.js
+++ components/property.js
@@ -12,14 +12,14 @@ const Properties = () => (
     <div className="row d-flex">
 
   <Link href="https://www.idealista.com/pro/unico-hogar/venta-viviendas/" passHref >
-    <div className=" banner d-flex align-items-center justify-content-center col-12 col-md-6 p-3">
+    <div className=" banner shadow-lg d-flex align-items-center justify-content-center col-12 col-md-6 p-3">
       <h3 className="text-center text-capitalize text-hogar2">Buy</h3>
     </div>
   </Link>
   
   
   <Link href="https://www.idealista.com/pro/unico-hogar/alquiler-viviendas/" passHref >
-    <div className=" banner d-flex align-items-center justify-content-center col-12 col-md-6 p-3 bg-white">
+    <div className=" banner shadow-lg d-flex align-items-center justify-content-center col-12 col-md-6 p-3 bg-white">
       <h3 className="text-center text-capitalize text-hogar2">Rent</h3>
     </div>
   </Link>
