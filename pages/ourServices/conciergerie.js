44afaf3389bc8f09944cc9e475b02118a3750641
Leonard <lonardlator@yahoo.fr>
2022-07-08 00:23:14 +0200

    🐛 attempt

--- pages/ourServices/conciergerie.js
+++ pages/ourServices/conciergerie.js
@@ -152,7 +152,7 @@ export default Conciergerie;
 
 export const getStaticProps = async ({ locale }) => ({
   props: {
-    locale: locale,
+    locale: locale || "",
     // t: t,
     // lang: lang,
   },
