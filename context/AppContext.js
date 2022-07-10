ed31d7b56a5f5b95e07aa903129aaffc16483f4e
Leonard <lonardlator@yahoo.fr>
2022-07-10 12:59:29 +0200

    🐛 attempt

--- context/AppContext.js
+++ context/AppContext.js
@@ -10,7 +10,7 @@ export const ContextProvider = ({ children }) => {
   const { locale } = useRouter();
 
   // assign t as either of the translation json files localted in '../locales' according to the current locale
-  let t = (tType = locale === "en" ? en : fr);
+  let t = locale === "es" ? es : en;
 
   // create app context to retrieve t value across the app
   const Context = createContext(t);
