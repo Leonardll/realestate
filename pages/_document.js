d6a5e3aa34984726329ac1574db4c933dc705a3d
Leonard <lonardlator@yahoo.fr>
2022-07-10 11:45:51 +0200

    🐛 attempt

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,16 @@
-import Document, { Html, Head, Main, NextScript } from "next/document"
-
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { i18n } from "next-i18next";
 export default class CustomDocument extends Document {
-    render() {
-        return <Html lang="es">
-            <Head>
-
-            </Head>
-            <body>
-                <Main />
-            </body>
-            <NextScript />
-        </Html>
-    }
+  render() {
+    const currentLocale = this.props.__NEXT_DATA__.locale || i18n.defaultLocale;
+    return (
+      <Html lang={currentLocale}>
+        <Head></Head>
+        <body>
+          <Main />
+        </body>
+        <NextScript />
+      </Html>
+    );
+  }
 }
