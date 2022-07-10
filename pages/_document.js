12eee8d9054bb7c271d89d4599819b9be8b932d7
Leonard <lonardlator@yahoo.fr>
2022-07-10 11:52:11 +0200

    🐛 attempt

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { i18n } from "next-i18next";
+import i18n from "../next.config";
+
 export default class CustomDocument extends Document {
   render() {
     const currentLocale = this.props.__NEXT_DATA__.locale || i18n.defaultLocale;
