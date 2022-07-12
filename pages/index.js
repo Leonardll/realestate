d651f16642e5fead651601f1c65c57678486c031
Leonard <lonardlator@yahoo.fr>
2022-07-12 09:00:38 +0200

    🐛

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useTranslation, Trans } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { Services } from "../components/Services";
 import { Contact } from "../components/contact/Contact";
 import { AboutUs } from "../components/AboutUs";
