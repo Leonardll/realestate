328146fbb841c3d3cbf42add2cffe41eeb61d327
Leonard <lonardlator@yahoo.fr>
2022-07-12 23:42:16 +0200

    🐛

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   i18n: {
     defaultLocale: "es-ES",
     locales: ["es-ES", "en-US"],
-    localeDetection: true,
+    localeDetection: false,
     reloadOnPrerender: process.env.NODE_ENV === "development",
     //react: { useSuspense: false },
     localeSubpaths: {
